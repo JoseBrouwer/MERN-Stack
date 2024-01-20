d917d8e6d531954fb62abad93ed24de15d7ed487
JoseBrouwer <jbb20b@fsu.edu>
2024-01-20 16:38:48 -0500

    Rating Component implemented!

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import { Card, CardBody } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import Rating from './Rating'
 
 const product = ({product}) => {
   return (
     <Card className="my-3 p-3 rounded">
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </Link>
+      <CardBody>
         <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant="top" />
+          <Card.Title as="div" className="product-title">
+            <strong>{product.name}</strong>
+          </Card.Title>
         </Link>
-        <CardBody>
-            <Link to={`/product/${product._id}`}>
-                <Card.Title as="div">
-                    <strong>{product.name}</strong>
-                </Card.Title>
-            </Link>
 
-            <Card.Text as="h3">
-                ${product.price}
-            </Card.Text>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} Reviews`}
+          />
+        </Card.Text>
 
-        </CardBody>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </CardBody>
     </Card>
-  )
+  );
 }
 
 export default product

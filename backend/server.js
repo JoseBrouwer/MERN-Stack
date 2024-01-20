06cae0cd7f7694e4ef5590c220620f970295b627
JoseBrouwer <jbb20b@fsu.edu>
2024-01-20 17:36:28 -0500

    Backend Express Server

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import products from "./data/products.js" //extension needed in backend for own modules
+const port = 5000; //different port for backend
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("API is running...")
+ });
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.send(product)
+});
+
+ app.listen(port, () => console.log(`Server running on port ${port}`))

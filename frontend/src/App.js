1be73e3abd2443ac7853cdfd7c9b1c91472b5978
JoseBrouwer <jbb20b@fsu.edu>
2024-01-20 15:40:30 -0500

    M5, 6, & 7. Header, Footer, CSS.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Container} from "react-bootstrap";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
-    <h1>Welcome to the MERN STACK</h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to the MERN STACK</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </>
+  );
+};
 
-export default App
+export default App;

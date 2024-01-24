83f5ab1244aeb411293199ebc73f2017d64ecb52
JoseBrouwer <jbb20b@fsu.edu>
2024-01-24 18:18:40 -0500

    25. Custom Errors

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,12 +8,19 @@ router.get('/', asyncHandler(async (req, res) => {
     res.json(products);
 }));
 
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if(product) {
-        return res.json(product);
-    }
-    res.status(404).json({message: 'Product not found'});
-}));
+router.get(
+    '/:id', 
+    asyncHandler(async (req, res) => {
+        const product = await Product.findById(req.params.id)
+        
+        if(product) {
+            return res.json(product);
+        }
+        else {
+            res.status(404);
+            throw new Error(`Resource Not Found`);
+        }
+    })
+);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import ConnectDB from "./config/db.js";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000; //different port for backend
 
@@ -14,5 +15,8 @@ app.get('/', (req, res) => {
  });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`))

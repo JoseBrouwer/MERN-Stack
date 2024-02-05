c38d551d551eba54dfe7bef0b44886e01b8fe8d7
JoseBrouwer <jbb20b@fsu.edu>
2024-02-04 19:54:00 -0500

    45. User Register Endpoint & Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,28 +1,17 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
 import jwt from 'jsonwebtoken';
+import generateToken from '../utils/generateToken.js';
 
 // @desc Auth user
 // @route POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    const {email, password} = req.body
+    const {email, password} = req.body;
     const user = await User.findOne({ email });
 
     if(user && (await user.matchPassword(password))) {
-    const token = jwt.sign({userId: user._id}, 
-        process.env.JWT_SECRET, 
-        {
-            expiresIn: '30d'
-        });
-
-        //Set JWT as HTTP only cookie
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development', 
-            sameSite: 'strict', 
-            maxAge: 30 * 24 * 60 * 60 * 1000 //30 days
-        });
+        generateToken(res, user._id);
 
         res.json({
             _id: user._id,
@@ -40,7 +29,35 @@ const authUser = asyncHandler(async (req, res) => {
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.send('register user');
+    const {name, email, password} = req.body;
+
+    const userExists = await User.findOne({email});
+
+    if(userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    } else {
+        const user = await User.create({
+            name, 
+            email, 
+            password
+        });
+
+        if(user) {
+            generateToken(res, user._id)
+
+            res.status(201).json({
+                _id: user._id,
+                name: user.name, 
+                email: user.email, 
+                isAdmin: user.isAdmin
+            });
+        } else {
+            res.status(400);
+            throw new Error('Invalid use data');
+        }
+    }
+    
 });
 
 // @desc logout user / clear cookie

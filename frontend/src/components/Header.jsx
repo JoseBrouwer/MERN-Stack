f10e7c91f15f2c2a33e9d5aac5e28cbc5b15d537
JoseBrouwer <jbb20b@fsu.edu>
2024-02-05 21:37:42 -0500

    50. User Logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import {Navbar, Nav, Container, Badge, NavDropdown} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
 import {LinkContainer} from 'react-router-bootstrap';
-import {useSelector} from 'react-redux';
-import logo from '../assets/logo.png'
+import {useSelector, useDispatch} from 'react-redux';
+import {useLogoutMutation} from '../slices/usersApiSlice';
+import {logout} from '../slices/authSlice';
+import logo from '../assets/logo.png';
 
 
 const Header = () => {
   const {cartItems} = useSelector((state) => state.cart);
   const {userInfo} = useSelector((state) => state.auth);
 
-  const logoutHandler = () => {
-    console.log('logout');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login');
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (

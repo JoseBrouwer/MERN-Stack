4b52e937f36ff6b65f8375305949e16c02fa7dd8
JoseBrouwer <jbb20b@fsu.edu>
2024-02-16 20:36:39 -0500

    66 & 67. Admin Route Component & Order List

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import {Navbar, Nav, Container, Badge, NavDropdown} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
 import {LinkContainer} from 'react-router-bootstrap';
@@ -32,7 +32,6 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          
           <LinkContainer to="/">
             <Navbar.Brand>
               <img src={logo} alt="ProShop" />
@@ -42,46 +41,53 @@ const Header = () => {
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            
             <Nav className="ms-auto">
-              
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <FaShoppingCart /> Cart
-                  {
-                    cartItems.length > 0 && (
-                      <Badge pill bg='success' 
-                      style={{marginLeft: '5px'}}>
-                          {/* a = accumulator, c = cart */}
-                          {cartItems.reduce((a, c) => a + c.qty, 0)}
-                      </Badge>
-                    )
-                  }
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {/* a = accumulator, c = cart */}
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
-              
-              {userInfo ? 
-              (
-                <NavDropdown title={userInfo.name} id='username'>
-                  <LinkContainer to='/profile'>
+
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
                   <NavDropdown.Item onClick={logoutHandler}>
                     Logout
                   </NavDropdown.Item>
                 </NavDropdown>
-              ) : 
-              (
+              ) : (
                 <LinkContainer to="/login">
-                <Nav.Link href="/login">
-                  <FaUser /> Sign In{" "}
-                </Nav.Link>
-              </LinkContainer>
+                  <Nav.Link href="/login">
+                    <FaUser /> Sign In{" "}
+                  </Nav.Link>
+                </LinkContainer>
               )}
-              
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
 
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+
+                  <LinkContainer to="/admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+
+                </NavDropdown>
+              )}
             </Nav>
-
           </Navbar.Collapse>
         </Container>
       </Navbar>

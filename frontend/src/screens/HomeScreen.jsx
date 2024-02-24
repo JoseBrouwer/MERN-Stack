6f2f0d27d1dc6f5621458823e89a1cd170600d65
JoseBrouwer <jbb20b@fsu.edu>
2024-02-23 23:28:02 -0500

    82 & 83. Pagination

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
+import { useParams } from "react-router-dom";
 import Product from '../components/Product.jsx';
 import { useGetProductsQuery } from '../slices/productsApiSlice.js';
 import Loader from '../components/Loader';
 import Message from '../components/Message.jsx';
+import Paginate from '../components/Paginate.jsx';
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error} = useGetProductsQuery();
+  const { pageNumber } = useParams();
+
+  console.log(pageNumber);
+
+  const { data, isLoading, error } = useGetProductsQuery({
+    pageNumber,
+  });
 
   return (
     <>
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant='danger'>{error?.data?.message || error.error}</Message>
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               //Determines amount of items based on screen size
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
             ))}
           </Row>
+          <Paginate 
+            pages={data.pages} 
+            page={data.page} 
+          />
         </>
       )}
     </>
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,6 +1,8 @@
 import { LinkContainer } from "react-router-bootstrap";
 import { Table, Button, Row, Col } from "react-bootstrap";
 import { FaEdit, FaPlus, FaTrash } from "react-icons/fa";
+import { useParams } from "react-router-dom";
+import Paginate from "../../components/Paginate";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import { 
@@ -11,7 +13,8 @@ import {
 import { toast } from "react-toastify";
 
 const ProductListScreen = () => {
-  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error, refetch } = useGetProductsQuery( {pageNumber} );
 
   const [createProduct, { isLoading: loadingCreate }] =
     useCreateProductMutation();
@@ -76,7 +79,7 @@ const ProductListScreen = () => {
               </tr>
             </thead>
             <tbody>
-              {products.map((product) => (
+              {data.products.map((product) => (
                 <tr key={product._id}>
                   <td>{product._id}</td>
                   <td>{product.name}</td>
@@ -101,7 +104,7 @@ const ProductListScreen = () => {
               ))}
             </tbody>
           </Table>
-          {/* PAGINATE PLACEHOLDER */}
+          <Paginate pages={data.pages} page={data.page} isAdmin={true} />
         </>
       )}
     </>

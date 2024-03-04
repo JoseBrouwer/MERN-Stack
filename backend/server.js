aef36998d6705ad44f11ede0a9db55effb40fc9b
JoseBrouwer <jbb20b@fsu.edu>
2024-03-04 13:30:54 -0500

    moved 'dotenv' for testing

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 import path from 'path';
 import express from 'express';
-import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-dotenv.config();
 import ConnectDB from "./config/db.js";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -10,7 +8,10 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
+import dotenv from 'dotenv';
 const port = process.env.PORT || 5000; //different port for backend
+
+dotenv.config();
 
 connectDB(); //connect to MongoDB
 const app = express();

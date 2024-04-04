cf0ddce785dd3e0505c6afc0f43d34a6a20912c7
MaverickHS <maverickhasc@gmail.com>
2024-04-03 20:18:19 -0400

    UserEditScreen.jsx -- imported multiple times.

--- frontend/src/screens/admin/UserEditScreen.jsx
+++ frontend/src/screens/admin/UserEditScreen.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import FormContainer from "../../components/FormContainer";
 import { toast } from "react-toastify";
-import { useParams } from "react-router-dom";
 import {
   useGetUserDetailsQuery,
   useUpdateUserMutation,

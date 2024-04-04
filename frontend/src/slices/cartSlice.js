d4e2a77d5a2fe288bc870feafef6a9aebd754325
Jose <josebrouwer1@gmail.com>
2024-04-04 18:13:43 -0400

    Removed useless assignment to variable "state"

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -44,7 +44,7 @@ const cartSlice = createSlice({
             //localStorage.setItem('cart', JSON.stringify(state));
             
         },
-        resetCart: (state) => (state = initialState),
+        resetCart: () => initialState,
     },
 });
 
